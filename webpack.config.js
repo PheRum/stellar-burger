bd5d42bdf051750759a3158242f3e3f23dfa0f32
Valery Malyshev <pherum83@gmail.com>
2024-05-28 00:16:37 +0300

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, "./dist"),
-        filename: "bundle.js",
+        filename: "bundle.js?[contenthash]",
     },
     devServer: {
         static: path.join(__dirname, "./dist"),
